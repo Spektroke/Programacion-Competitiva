1a0a2f4d8a8e7c14f122f77a7e4dc6bd3a802fa5
Alejandro Colque <95255576+Spektroke@users.noreply.github.com>
2022-05-23 19:12:24 -0300

    Add files via upload

---
+++ Ejemplo_if.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h> /// Contexto, lo veremos más adelante 
+
+using namespace std; /// Contexto, lo veremos más adelante 
+
+int main(){ /// Entre estas llaves debemos escribir nuestro código
+	int x,y;
+	cin>>x>>y;
+	if(x>y){
+		cout<<"x es mayor que y\n";
+	}else{
+		cout<<"x no es mayor que y\n";
+	}
+}
