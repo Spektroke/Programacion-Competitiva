9e4d2bf849aed1ff04dea16e70cecfd0cdac590c
Alejandro Colque <95255576+Spektroke@users.noreply.github.com>
2022-05-24 21:25:57 -0300

    Add files via upload

---
+++ Caracteres_y_strings.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h> /// Contexto, lo veremos más adelante 
+
+using namespace std; /// Contexto, lo veremos más adelante 
+
+int main(){ /// Entre estas llaves debemos escribir nuestro código
+	char c;
+	cin>>c;
+	cout<<"El contenido de c es "<<c<<'\n';
+	
+	string s;
+	cin>>s;
+	cout<<"El contenido de s es "<<s<<'\n';
+}
+ 
