8ce270afa6d8d4b694bc3d4a32d6c447eb8777aa
Alejandro Colque <95255576+Spektroke@users.noreply.github.com>
2022-05-24 21:25:39 -0300

    Add files via upload

---
+++ Solución_Solve_me_first.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h> /// Contexto, lo veremos más adelante 
+ 
+using namespace std; /// Contexto, lo veremos más adelante 
+ 
+int main(){ /// Entre estas llaves debemos escribir nuestro código
+	int a,b;
+	cin>>a>>b;
+	cout<<(a+b)<<"\n";
+}
+
